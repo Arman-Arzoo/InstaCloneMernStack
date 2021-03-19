2dc559e179350f926a1b5dfb921b73023cafcfeb
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2021-03-19 19:33:35 +0500

    create user model and update server.js

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
+// All use dependencies importing
 const express = require('express');
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+require('./model/userModel');
+
+// invoking the express Server
 const app = express();
 
-const {PORT,DB} = require("./config/keys")
+// destructure enviornmental variables
+const {PORT,DB} = require("./config/keys");
 
-app.listen(PORT,()=>{console.log("server is running on port",PORT)})
+// Start app listening server
+app.listen(PORT,()=>{console.log("server is running on port",PORT)});
+
+// connecion to Mongo Database 
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+});
+
+mongoose.connection.on("connected",()=>{console.log("connected to mongodb")});
+
+mongoose.connection.on("error",(err)=>{console.log("error connecting ",err)});
+

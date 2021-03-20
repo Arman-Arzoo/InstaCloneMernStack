724ab7d76b5ef15fc0fb1f4e4dfb98231b46809c
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2021-03-20 11:31:27 +0500

    signup user complete

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('user');
+const bcrypt = require('bcryptjs')
 
 exports.home = (req,res)=>{
     res.send("hello for home")
 }
 
 exports.signUp = async(req,res)=>{
-    const {name, email, password} = req.body
+    try {
+        const {name, email, password} = req.body
    
     if(!name || !email || !password){
         return res.status(422).json({msg:"please fill out the field"})
@@ -18,16 +20,23 @@ exports.signUp = async(req,res)=>{
         return res.status(422).json({msg:"User already exist"})
     }
 
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password,salt)
+
+    
     const newUser = new User({
         email,
-        password,
+        password:hashPassword,
         name
     })
 
    const savedUser = await newUser.save();
    res.status(200).json(savedUser)
+    } catch (err) {
+        
+    }
 
-//    res.status(200).json({msg:"successfuly save into the database"})
+ res.status(400).json({msg:"Unfortuanaly can't register user"})
     
 }
 

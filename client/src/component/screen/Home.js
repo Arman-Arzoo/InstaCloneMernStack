7c65578553fb08fcf578c504fe5b89fec0ade391
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2021-03-22 22:08:30 +0500

    Home for instagram post

--- client/src/component/screen/Home.js
+++ client/src/component/screen/Home.js
@@ -2,8 +2,84 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>
-            Home
+        <div >
+            <div className="card home-card">
+                <h5 className="home-content post_img">Asad</h5>
+                <div className="home-content">
+                    <img src ="https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533__340.jpg" alt="post"></img>
+                </div>
+                <div className="card-contents home-content">
+                    <h6>tittle</h6>
+                    <p>Wow amazing pos</p>
+                    <input type="text" placeholder="add a comment"></input>
+                </div>
+
+            </div>
+
+
+
+            <div className="card home-card">
+                <h5 className="home-content post_img">Asad</h5>
+                <div className="home-content">
+                    <img src ="https://cdn.pixabay.com/photo/2017/04/09/09/56/avenue-2215317__340.jpg" alt="post"></img>
+                </div>
+                <div className="card-contents home-content">
+                    <h6>tittle</h6>
+                    <p>Wow amazing pos</p>
+                    <input type="text" placeholder="add a comment"></input>
+                </div>
+
+            </div>
+
+
+
+            <div className="card home-card">
+                <h5 className="home-content post_img">Asad</h5>
+                <div className="home-content">
+                    <img src ="https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533__340.jpg" alt="post"></img>
+                </div>
+                <div className="card-contents home-content">
+                    <h6>tittle</h6>
+                    <p>Wow amazing pos</p>
+                    <input type="text" placeholder="add a comment"></input>
+                </div>
+
+            </div>
+
+
+
+
+            <div className="card home-card">
+                <h5 className="home-content">Asad</h5>
+                <div className="home-content post_img">
+                    <img src ="https://cdn.pixabay.com/photo/2018/09/23/18/30/drop-3698073__340.jpg" alt="post"></img>
+                </div>
+                <div className="card-contents home-content">
+                    <h6>tittle</h6>
+                    <p>Wow amazing pos</p>
+                    <input type="text" placeholder="add a comment"></input>
+                </div>
+
+            </div>
+
+
+
+
+            <div className="card home-card">
+                <h5 className="home-content">Asad</h5>
+                <div className="home-content post_img">
+                    <img src ="https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533__340.jpg" alt="post"></img>
+                </div>
+                <div className="card-contents home-content">
+                    <h6>tittle</h6>
+                    <p>Wow amazing pos</p>
+                    <input type="text" placeholder="add a comment"></input>
+                </div>
+
+            </div>
+
+
+
         </div>
     )
 }
